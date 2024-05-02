1f6b5e82ff50cce7477ac77bbdb47c9501af3271
admiralbolt <aviknecht@gmail.com>
2024-05-01 17:54:32 -0700

    Emboss!

--- vis/draw/misc/emboss.py
+++ vis/draw/misc/emboss.py
@@ -2,38 +2,35 @@ import argparse
 
 import cv2
 
-def emboss(plate_file, stamp_file, output_file):
-  plate = cv2.imread(plate_file)
-  stamp = cv2.imread(stamp_file)
-  height, width, channels = stamp.shape
-  
-  # Create a mask of the stamp that is just the good bits.
-  # This will probably need to be file dependent.
-  gray = cv2.cvtColor(stamp, cv2.COLOR_BGR2GRAY)
-  _, stamp_mask = cv2.threshold(gray, 160, 255, cv2.THRESH_BINARY)
-  stamp_mask = cv2.bitwise_not(stamp_mask)
-  cv2.imshow("stamp_mask", stamp_mask)
 
-  # Get a place slice that matches the stamp size.
-  background = plate[350:350+height, 30:30+width]
-  cv2.imshow("background", background)
+def emboss(background_image, foreground_image, output_image):
+  background = cv2.imread(background_image)
+  foreground = cv2.imread(foreground_image)
+  height, width, channels = foreground.shape
 
-  mix = cv2.addWeighted(stamp, 0.6, background, 0.4, 0)
-  foreground = cv2.bitwise_and(mix, mix, mask=stamp_mask)
-  background = cv2.bitwise_and(background, background, mask=cv2.bitwise_not(stamp_mask))
-  cv2.imshow("mix", mix)
+  background = background[:height, :width]
 
-  final = cv2.add(foreground, background)
+  # Mask our foreground image.
+  foreground_gray = cv2.cvtColor(foreground, cv2.COLOR_BGR2GRAY)
+  foreground_invert = cv2.bitwise_not(foreground_gray)
+  _, foreground_mask = cv2.threshold(foreground_invert, 40, 255, cv2.THRESH_BINARY)  
+
+  mix = cv2.addWeighted(foreground, 0.6, background, 0.4, 0)
+
+  mix_masked = cv2.bitwise_and(mix, mix, mask=foreground_mask)
+  background_masked = cv2.bitwise_and(background, background, mask=cv2.bitwise_not(foreground_mask))
+
+  final = cv2.add(mix_masked, background_masked)
   cv2.imshow("final", final)
-
   cv2.waitKey(0)
 
 
-if __name__ == "__main__":
-  parser = argparse.ArgumentParser(description="Test effect.")
-  parser.add_argument("--plate_file", type=str, help="Background plate")
-  parser.add_argument("--stamp_file", type=str, help="Stamp")
-  parser.add_argument("--output_file", type=str, help="Output file")
-  args = parser.parse_args()
 
-  emboss(args.plate_file, args.stamp_file, args.output_file)
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description="")
+  parser.add_argument("--background", type=str, help="Background image.")
+  parser.add_argument("--foreground", type=str, help="Foreground image.")
+  parser.add_argument("--output", type=str, help="Output image.")
+  args = parser.parse_args()
+  
+  emboss(args.background, args.foreground, args.output)
